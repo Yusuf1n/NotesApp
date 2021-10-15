abdc01ae79352610ac07009dbb5f144135dcf484
Yusuf Naheem <yusufnaheem1@gmail.com>
2021-10-15 18:05:07 +0100

    Implementing add and edit functionality

--- src/components/NotesTable.js
+++ src/components/NotesTable.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { DeleteNote, GetNotes } from "../services/notes";
 import { Button } from "react-bootstrap";
+import { EditNoteModal } from "./NoteModal";
 
 export const NotesTable = () => {
   const notes = useSelector((state) => state.notesReducer.notes);
@@ -16,6 +17,9 @@ export const NotesTable = () => {
       <tbody>
         {notes.map((n) => (
           <tr>
+            <td style={{ width: "3rem" }}>
+              <EditNoteModal note={n.value} />
+            </td>
             <td style={{ width: "3rem" }}>
               <Button
                 className="btn btn-danger"
--- src/redux/notesReducer.js
+++ src/redux/notesReducer.js
@@ -5,11 +5,15 @@ const initialState = {
 export const ActionTypes = {
   SET_NOTES: "SET_NOTES",
   DELETE_NOTE: "DELETE_NOTE",
+  NEW_NOTE: "NEW_NOTE",
+  EDIT_NOTE: "EDIT_NOTE",
 };
 
 export const ActionCreators = {
   setNotes: (payload) => ({ type: ActionTypes.SET_NOTES, payload }),
   deleteNote: (payload) => ({ type: ActionTypes.DELETE_NOTE, payload }),
+  newNote: (payload) => ({ type: ActionTypes.NEW_NOTE, payload }),
+  editNote: (payload) => ({ type: ActionTypes.EDIT_NOTE, payload }),
 };
 
 export default function NotesReducer(state = initialState, action) {
@@ -17,10 +21,19 @@ export default function NotesReducer(state = initialState, action) {
     case ActionTypes.SET_NOTES:
       return { ...state, notes: [...action.payload] };
     case ActionTypes.DELETE_NOTE:
-      for (let i = 0; state.notes.length; i++) {
+      for (let i = 0; i < state.notes.length; i++) {
         if (state.notes[i].id === action.payload.id) {
           state.notes.splice(i, 1);
           break;
+        }
+      }
+      return { ...state, notes: [...state.notes] };
+    case ActionTypes.NEW_NOTE:
+      return { ...state, notes: [...state.notes, action.payload] };
+    case ActionTypes.EDIT_NOTE:
+      for (let i = 0; i < state.notes.length; i++) {
+        if (state.notes[i].id === action.payload.id) {
+          state.notes[i].value = action.payload.value;
         }
       }
       return { ...state, notes: [...state.notes] };
